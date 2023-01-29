997807854eb566f54a96bdd562c809a70e4117d3
yorickshan <yorickshan@gmail.com>
2023-01-29 13:51:56 +0800

    fix: base url

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
-import { createApp } from 'vue';
-import './tailwind.css';
-import App from './App.vue';
-import { routes } from './routes';
-import { createRouter, createWebHistory } from 'vue-router';
+import { createApp } from 'vue'
+import './tailwind.css'
+import App from './App.vue'
+import { routes } from './routes'
+import { createRouter, createWebHistory } from 'vue-router'
 
-const app = createApp(App);
+const app = createApp(App)
 
 const router = createRouter({
-  history: createWebHistory(),
+  // In the Vue Router 4, set base path by history api
+  history: createWebHistory(import.meta.env.VITE_APP_BASE_URL),
   routes,
-});
+})
 
-app.use(router);
-app.mount('#app');
+app.use(router)
+app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return defineConfig({
+    plugins: [vue()],
+    base: env.VITE_APP_BASE_URL,
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
+      },
     },
-  },
-  server: {
-    open: true,
-  },
-})
+    server: {
+      open: true,
+    },
+  })
+}
